ccd67416445738b35e185d5c27ca1b1f19ba71f4
Ouyang Yingjie <ouyang_yingjie@outlook.com>
2015-11-27 00:59:33 +0800

    src/calculate-second.js_Rec_creating some functions to make the code cleaner 

--- src/calculate-second.js
+++ src/calculate-second.js
@@ -6,24 +6,32 @@ function sum(arr){
 }
 
 
-function calculate_median(arr) {
-    var temp_arr = [];
-    var median;
+function arr_find_odd_NO_elements(arr){
+    var arr_odd_NO_elements = [];
     arr.filter(function(item,index){
-        if(index % 2 !== 0) {
-            temp_arr.push(item);
-        }
+        if(index % 2 !== 0)
+            arr_odd_NO_elements.push(item);
     });
+    return arr_odd_NO_elements;
+}
 
-
-    var sorted_temp_arr = temp_arr.sort();
-    if(sorted_temp_arr.length % 2 !== 0) {
-        var median_index = Math.floor(sorted_temp_arr.length / 2);
-        median = sorted_temp_arr[median_index];
+function arr_find_median(arr){
+    var median;
+    if(arr.length % 2 !== 0) {
+        var median_index = Math.floor(arr.length / 2);
+        median = arr[median_index];
     }
     else{
-         median = sum(sorted_temp_arr) / sorted_temp_arr.length;
+         median = sum(arr) / arr.length;
     }
+    return median;
+}
+
+function calculate_median(arr) {
+    var median;
+    var arr_odd_NO_elements = arr_find_odd_NO_elements(arr);
+    var sorted_arr_odd_NO_elements = arr_odd_NO_elements.sort();
+    median = arr_find_median(sorted_arr_odd_NO_elements);
     return median;
 }
 
